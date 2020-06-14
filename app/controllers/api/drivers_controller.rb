712d07139f2d04408c38997aeb7df20dd832ba9b
Alexandre Wiggert <alexandrewiggert@gmail.com>
2020-06-14 10:54:20 -0300

    Fixes create on DriverController API

--- app/controllers/api/drivers_controller.rb
+++ app/controllers/api/drivers_controller.rb
@@ -2,7 +2,7 @@ module Api
   class DriversController < ApplicationController
 
     def create
-      @driver = Driver.create_or_find_by(params[:messenger_id]) do |driver|
+      @driver = Driver.create_or_find_by(messenger_id: params[:messenger_id]) do |driver|
           driver.assign_attributes(driver_params)
       end
       render json: @driver, status: :created
